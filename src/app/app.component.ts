e00c9c6b804997e46de95048c523bfc157b567b2
Arshjeet2003 <arshjeetsingh123@gmail.com>
2024-10-02 14:15:14 +0530

    feat(codershub): add footer
    This commit adds footer for the landing page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoComponent } from './logo/logo.component';
-
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, CommonModule, NavbarComponent, LogoComponent],
+  imports: [RouterOutlet, NgbModule, CommonModule, NavbarComponent, LogoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
