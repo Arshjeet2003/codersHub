21eec44117dd7d798729652df385ab42d323749f
Arshjeet2003 <arshjeetsingh123@gmail.com>
2024-10-02 14:08:53 +0530

    feat(codershub): add navbar
    Added navbar to the landing page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, CommonModule],
+  imports: [RouterOutlet, NgbModule, CommonModule, NavbarComponent, LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
